3965ad4d1f27b8ad9ce1508bfe004879d5f35984
MariaAqeel <168283683+MariaAqeel@users.noreply.github.com>
2024-04-29 20:33:59 +0500

    Add files via upload

--- kd_tree.py
+++ kd_tree.py
@@ -6,7 +6,7 @@ def insert(root, filename, point, depth=0):
     k = len(point)
     axis = depth % k
     
-    if point[axis] <= root['point'][axis]:
+    if point[axis] < root['point'][axis]:
         root['left'] = insert(root['left'], filename, point, depth + 1)
     else:
         root['right'] = insert(root['right'], filename, point, depth + 1)
@@ -46,21 +46,36 @@ def deleteNode(root, point, depth=0):
         elif root['right'] is None:
             return root['left']
         else:
-            if point[0] == root['point'][0] and point[1] == root['point'][1] and point[2] == root['point'][2]:
-                temp = minValueNode(root['right'])
-                root['point'] = temp['point']
-                root['filename'] = temp['filename']
-                root['right'] = deleteNode(root['right'], temp['point'], depth + 1)
-            else:
-                if point[axis] < root['point'][axis]:
-                    root['left'] = deleteNode(root['left'], point, depth + 1)
-                elif point[axis] > root['point'][axis]:
-                    root['right'] = deleteNode(root['right'], point, depth + 1)
+            temp = minValueNode(root['right'])
+            root['point'] = temp['point']
+            root['filename'] = temp['filename']
+            root['right'] = deleteNode(root['right'], temp['point'], depth + 1)
 
     return root
 
 
 
 
+# def write_kd_tree_to_file(kd_tree, file_path):
+#     with open(file_path, 'w') as f:
+#         # Use a recursive function to write the kd-tree structure
+#         def write_node(root, indent=0):
+#             if root is None:
+#                 return
+            
+#             # Write current root's data (point and filename)
+#             f.write(' ' * indent + f"point: {root['point']}, filename: '{root['filename']}'\n")
+            
+#             # Recursively write left and right subtrees
+#             write_node(root['left'], indent + 2)
+#             write_node(root['right'], indent + 2)
+        
+#         # Start writing from the root root
+#         write_node(kd_tree)
+
+# # Example usage to write the kd-tree to a text file
+# kd_tree = {...}  # Your kd-tree dictionary
+# file_path = 'kd_tree.txt'
+# write_kd_tree_to_file(kd_tree, file_path)
 
 
